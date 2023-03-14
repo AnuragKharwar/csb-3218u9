cc8ffa1d903fe3ae2b085f9a72ec44bedde41a4b
Code Sandbox <hello@codesandbox.io>
2023-03-14 16:15:43 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.b7e0dd5f96427447e3a65348ad52c6c0.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "798f1d92441aaf9dd82eb200179fb649",
+    "url": "https://anuragkharwar.github.io/csb-3218u9/index.html"
+  },
+  {
+    "revision": "7065aa2ced885b05a63b",
+    "url": "https://anuragkharwar.github.io/csb-3218u9/static/js/2.75179000.chunk.js"
+  },
+  {
+    "revision": "7a63305796e9d5557dd1",
+    "url": "https://anuragkharwar.github.io/csb-3218u9/static/js/main.588e5301.chunk.js"
+  },
+  {
+    "revision": "8c7d64807a09a82d691e",
+    "url": "https://anuragkharwar.github.io/csb-3218u9/static/js/runtime-main.6ad5d32a.js"
+  }
+]);
